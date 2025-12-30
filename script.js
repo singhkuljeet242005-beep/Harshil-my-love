bce499d9cb37fbe94bca51b7574f4f4e023dca18
singhkuljeet242005-beep <singhkuljeet242005@gmail.com>
2025-12-30 16:15:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function goToPage(pageNumber) {
+  document.querySelector('.page.active').classList.remove('active');
+  document.getElementById(`page${pageNumber}`).classList.add('active');
+}
 
+function surprise(el) {
+  el.innerText = "💫";
+  el.style.transform = "scale(1.5)";
+  setTimeout(() => {
+    el.innerText = "💖";
+    el.style.transform = "scale(1)";
+  }, 600);
+}
